31c3ba32206d688a41d6caf49a5e29b6da8838d0
StarNovaBR <cherrylegal001@gmail.com>
2026-05-02 13:38:00 -0300

    sorry

--- source/funkin/states/substates/GameplayChangersSubstate.hx
+++ source/funkin/states/substates/GameplayChangersSubstate.hx
@@ -289,7 +289,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 				}
 			}
 			
-			if (controls.RESET #if || virtualPad.buttonC.justPressed #end)
+			if (controls.RESET #if mobile || virtualPad.buttonC.justPressed #end)
 			{
 				for (i in 0...optionsArray.length)
 				{

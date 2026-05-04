d93d43e20503bd705f280d6049e5e4bb6cb4c69e
StarNovaBR <cherrylegal001@gmail.com>
2026-05-04 16:08:54 -0300

    test

--- source/funkin/states/ModsState.hx
+++ source/funkin/states/ModsState.hx
@@ -90,12 +90,12 @@ class ModsState extends MusicBeatState
 		
 		add(new FlxSprite().loadGraphic(Paths.image("menus/mods/menuborder1")));
 		add(new FlxSprite(685, 645).loadGraphic(Paths.image("menus/mods/menuborder2")));
-		
-        #if mobile
-        addVirtualPad(UP_DOWN, A_B);
-        #end
         
 		changeDir(0);
+		
+		#if mobile
+        addVirtualPad(UP_DOWN, A_B);
+        #end
     }
 	override public function update(elapsed)
 	{
